37c18d5c2a72f1d192b8c7616cc1cf9e275f3082
almondheng <emperorpointguard@gmail.com>
2025-06-15 17:56:30 +0800

    add homework 1

---
+++ homework-1/deepseek_v3_tokenizer/deepseek_tokenizer.py
@@ -0,0 +1,9 @@
+# pip3 install transformers
+# python3 deepseek_tokenizer.py
+import transformers
+
+chat_tokenizer_dir = "./deepseek_v3_tokenizer/"
+
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+    chat_tokenizer_dir, trust_remote_code=True
+)
